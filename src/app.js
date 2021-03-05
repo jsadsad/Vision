70e5e90131b8df745a58e99669a6502fb97c85b0
JCS <joshsad2@gmail.com>
2021-03-04 22:24:43 -0800

    styles

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
-const rectWidth = 100
+const rectWidth = 225
 const svgHeight = 250
-const t = d3.transition().duration(1000)
+const t = d3.transition().duration(2000)
 
-let xscale = d3.scaleLinear().domain([0, 500]).range([0, 250])
+// let xscale = d3.scaleLinear().domain([0, 100]).range([0, 100])
 
 let yscale = d3.scaleLinear().domain([3, 0]).range([0, 250])
-
-let x_axis = d3.axisBottom().scale(xscale)
 
 let y_axis = d3
   .axisLeft()
@@ -15,6 +13,34 @@ let y_axis = d3
   .tickFormat((d) => {
     if (d !== 0) return d + 'billion'
   })
+
+// let x_axis = d3.axisBottom().scale(xscale)
+
+let svg = d3.select('body').append('svg').attr('width', 500).attr('height', 300)
+
+let defs = svg.append('defs')
+
+let gradient = defs
+  .append('linearGradient')
+  .attr('id', 'svgGradient')
+  .attr('x1', '0%')
+  .attr('x2', '100%')
+  .attr('y1', '0%')
+  .attr('y2', '100%')
+
+gradient
+  .append('stop')
+  .attr('class', 'start')
+  .attr('offset', '0%')
+  .attr('stop-color', 'rgb(214,183,33)')
+  .attr('stop-opacity', 2)
+
+gradient
+  .append('stop')
+  .attr('class', 'end')
+  .attr('offset', '100%')
+  .attr('stop-color', 'rgb(31,131,51)')
+  .attr('stop-opacity', 2)
 
 d3.json('movies.json', (d) => {
   return {
@@ -26,16 +52,6 @@ d3.json('movies.json', (d) => {
   }
 }).then((data) => {
   movieData = data
-
-  const sizeGross = d3
-    .scaleLinear()
-    .domain(d3.extent(movieData, (d) => d.gross))
-    .range([0, 250])
-
-  const sizeBudget = d3
-    .scaleLinear()
-    .domain(d3.extent(movieData, (d) => d.budget))
-    .range([0, 250])
 
   const size = d3.scaleLinear().domain([0, 2800]).range([0, 250])
 
@@ -52,256 +68,17 @@ d3.json('movies.json', (d) => {
     d3.select(svg.node())
       .selectAll('rect')
       .data([size(movieData[i].gross), size(movieData[i].budget)])
-      // .data([sizeGross(movieDasizeta[i].gross), sizeBudget(movieData[i].budget)])
-      // .data(Object.values(movieData[i]).slice(3))
       .enter()
       .append('rect')
       .transition(t)
       .attr('width', rectWidth)
-      .attr('x', (d, i) => i * rectWidth) // [0] = 100, [1] = 200
+      .attr('x', (d, i) => i * rectWidth)
       .attr('y', (d) => svgHeight - d)
       .attr('height', (d) => d)
-      .attr('fill', '#ed1d24')
-      .attr('stroke', 'gainsboro')
+      .attr('fill', 'url(#svgGradient)')
+      .attr('stroke', 'rgb(221,0,29)')
       .attr('stroke-width', 2)
 
-    d3.select(svg.node())
-      // .selectAll('rect')
-      .attr('transform', 'translate(50, 10)')
-      .call(y_axis)
+    d3.select(svg.node()).attr('transform', 'translate(150, 50)').call(y_axis)
   }
 })
-
-// const container = d3.selectAll('#app')
-// const svg = container
-//   .append('svg')
-//   .attr('height', svgHeight)
-//   .attr('width', rectWidth)
-//   .style('overflow', 'visible')
-
-// const buttonCode = container.append('div')
-// const button = buttonCode
-//   .append('button')
-//   .text('Marvel')
-//   .attr('class', 'button-code')
-// const code = buttonCode.append('code')
-
-// const updateBars = () => {
-//   const t = d3.transition().duration(1000)
-//   const data = _.times(_.random(3, 8), (i) => _.random(0, 100))
-//   d3.select(svg.node())
-//     .selectAll('rect')
-//     .data(data, (d) => d)
-//     .join(
-//       (enter) => {
-//         return enter
-//           .append('rect')
-//           .attr('x', (d, i) => i * rectWidth)
-//           .attr('height', 0)
-//           .attr('y', svgHeight)
-//           .attr('fill', '#ed1d24')
-//           .attr('stroke', 'gainsboro')
-//           .attr('stroke-width', 2)
-//       },
-//       (update) => update,
-//       (exit) => {
-//         exit.transition(t).attr('height', 0).attr('y', svgHeight)
-//       }
-//     ) // enter + update selection
-//     .attr('width', rectWidth)
-//     .transition(t)
-//     .attr('x', (d, i) => i * rectWidth)
-//     .attr('height', (d) => d)
-//     .attr('y', (d) => svgHeight - d)
-
-//   d3.select(code.node()).text(JSON.stringify(data).replace(/\,/g, ', '))
-// }
-// // d3.json('movies.json').then((movie) => {})
-// updateBars()
-// d3.select(button.node()).on('click', updateBars)
-
-// const testCode = container.append('div')
-// const tes = testCode.append('div').text('get shit done')
-// const tes2 = testCode.append('div').text('test')
-
-// function calculateData(movies) {
-//   return _.map(movies, (data, i) => {
-//     debugger
-//     return {
-//       title: data.title,
-//       releaseDate: data.released,
-//       rating: data.rating,
-//       gross: data.gross,
-//     }
-//   })
-// }
-
-// d3.json('movies.json', (d) => {
-//   return {
-//     title: d['title'],
-//     released: d['released'],
-//     rating: d['rating'],
-//     gross: d['budget'],
-//   }
-// }).then((data) => {
-//   movieData = data
-//   calculateData(movieData)
-//   // createVisualization(nutritionData[0], 0, true)
-//   // createNavLi(0)
-//   // createAnchor(0)
-
-//   // for (let i = 1; i < nutritionData.length; i++) {
-//   //   createVisualization(nutritionData[i], i)
-//   //   createNavLi(i)
-//   //   createAnchor(i)
-//   // }
-// })
-
-// const calculateData = (movieData, idx) => {
-//   const t = d3.transition().duration(1000)
-//   const margin = {
-//     top: 20,
-//     right: 30,
-//     bottom: 30,
-//     left: 40,
-//   }
-//   let width = 600 - margin.left - margin.right
-//   let height = 475 - margin.top - margin.bottom
-//   d3.select(svg.node())
-//     .selectAll('rect')
-//     .data(movieData[0], (d) => d)
-//     .join(
-//       (enter) => {
-//         return enter
-//           .append('rect')
-//           .attr('x', (d, i) => i * rectWidth)
-//           .attr('height', 0)
-//           .attr('y', svgHeight)
-//           .attr('fill', '#ed1d24')
-//           .attr('stroke', 'gainsboro')
-//           .attr('stroke-width', 2)
-//       },
-//       (update) => update,
-//       (exit) => {
-//         exit.transition(t).attr('height', 0).attr('y', svgHeight)
-//       }
-//     ) // enter + update selection
-//     .attr('width', rectWidth)
-//     .transition(t)
-//     .attr('x', (d, i) => i * rectWidth)
-//     .attr('height', (d) => d)
-//     .attr('y', (d) => svgHeight - d)
-
-//   d3.select(code.node()).text(JSON.stringify(movieData).replace(/\,/g, ', '))
-// }
-
-// d3.json('movies.json').then((data) => {})
-
-// const iron1 = d3.selectAll('#iron-1')
-// const svg2 = iron1
-//   .append('svg2')
-//   .attr('height', svgHeight)
-//   .attr('width', rectWidth)
-//   .style('overflow', 'visible')
-
-// const buttonCode = iron1.append('div')
-// const button1 = buttonCode
-//   .append('button')
-//   .text('Marvel')
-//   .attr('class', 'button-code')
-// const code1 = buttonCode.append('code')
-
-// const updateBars = () => {
-//   const t = d3.transition().duration(1000)
-//   const data = _.times(_.random(3, 8), (i) => _.random(0, 100))
-//   d3.select(svg2.node())
-//     .selectAll('rect')
-//     .data(data, (d) => d)
-//     .join(
-//       (enter) => {
-//         return enter
-//           .append('rect')
-//           .attr('x', (d, i) => i * rectWidth)
-//           .attr('height', 0)
-//           .attr('y', svgHeight)
-//           .attr('fill', '#ed1d24')
-//           .attr('stroke', 'gainsboro')
-//           .attr('stroke-width', 2)
-//       },
-//       (update) => update,
-//       (exit) => {
-//         exit.transition(t).attr('height', 0).attr('y', svgHeight)
-//       }
-//     ) // enter + update selection
-//     .attr('width', rectWidth)
-//     .transition(t)
-//     .attr('x', (d, i) => i * rectWidth)
-//     .attr('height', (d) => d)
-//     .attr('y', (d) => svgHeight - d)
-
-//   // d3.select(code1.node()).text(JSON.stringify(data).replace(/\,/g, ', '))
-// }
-// d3.json('movies.json').then((movie) => {})
-// updateBars()
-// d3.select(button1.node()).on('click', updateBars)
-
-// let hulk1 = d3.selectAll('#hulk-1')
-// const svg = hulk1
-//   .append('svg')
-//   .attr('height', svgHeight)
-//   .attr('width', rectWidth)
-//   .style('overflow', 'visible')
-
-// const buttonCode2 = hulk1.append('div')
-// const button2 = buttonCode2
-//   .append('button')
-//   .text('Marvel')
-//   .attr('class', 'button-code')
-// const code2 = buttonCode.append('code')
-
-// const updateBars2 = () => {
-//   const t = d3.transition().duration(1000)
-//   const data = _.times(_.random(3, 8), (i) => _.random(0, 100))
-//   d3.select(svg.node())
-//     .selectAll('rect')
-//     .data(data, (d) => d)
-//     .join(
-//       (enter) => {
-//         return enter
-//           .append('rect')
-//           .attr('x', (d, i) => i * rectWidth)
-//           .attr('height', 0)
-//           .attr('y', svgHeight)
-//           .attr('fill', '#ed1d24')
-//           .attr('stroke', 'gainsboro')
-//           .attr('stroke-width', 2)
-//       },
-//       (update) => update,
-//       (exit) => {
-//         exit.transition(t).attr('height', 0).attr('y', svgHeight)
-//       }
-//     ) // enter + update selection
-//     .attr('width', rectWidth)
-//     .transition(t)
-//     .attr('x', (d, i) => i * rectWidth)
-//     .attr('height', (d) => d)
-//     .attr('y', (d) => svgHeight - d)
-
-//   d3.select(code2.node()).text(JSON.stringify(data).replace(/\,/g, ', '))
-// }
-// // d3.json('movies.json').then((movie) => {})
-// updateBars2()
-// d3.select(button2.node()).on('click', updateBars2)
-
-// let svg1 = d3
-//   .select('.svgs')
-//   .append('svg')
-//   .attr('height', '150')
-//   .attr('width', '400')
-//   .attr('fill', '#222222')
-// let svg2 = d3
-//   .select('.svgs')
-//   .append('svg')
-//   .attr('height', '150')
-//   .attr('width', '400')
-//   .attr('fill', '#222222')
