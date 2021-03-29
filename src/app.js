101edf3f0220d8918231684d1050e5f577e4797f
Joshua C. Sadsad <joshsad2@gmail.com>
2021-03-28 20:52:37 -0700

    removed slow scroll

--- src/app.js
+++ src/app.js
@@ -1,35 +1,3 @@
-window.onload = function () {
-  window.scrollTo(0, 0)
-}
-
-if (window.addEventListener)
-  window.addEventListener('DOMMouseScroll', wheel, false)
-window.onmousewheel = document.onmousewheel = wheel
-
-function wheel(event) {
-  let delta = 0
-  if (event.wheelDelta) delta = event.wheelDelta / 120
-  else if (event.detail) delta = -event.detail / 3
-
-  handle(delta)
-  if (event.preventDefault) event.preventDefault()
-  event.returnValue = false
-}
-
-function handle(delta) {
-  let time = 1000
-  let distance = 300
-
-  $('html, body')
-    .stop()
-    .animate(
-      {
-        scrollTop: $(window).scrollTop() - distance * delta,
-      },
-      time
-    )
-}
-
 const createObs = (containers) => {
   let options = {
     root: null,
