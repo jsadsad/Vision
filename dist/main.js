4b08d5d56e72715563ad27fb9dd09f803339bd13
Joshua C. Sadsad <joshsad2@gmail.com>
2021-03-28 20:27:14 -0700

    added slow scroll

--- dist/main.js
+++ dist/main.js
@@ -7,6 +7,25 @@ window.onload = function () {
   window.scrollTo(0, 0);
 };
 
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+  var delta = 0;
+  if (event.wheelDelta) delta = event.wheelDelta / 120;else if (event.detail) delta = -event.detail / 3;
+  handle(delta);
+  if (event.preventDefault) event.preventDefault();
+  event.returnValue = false;
+}
+
+function handle(delta) {
+  var time = 1000;
+  var distance = 300;
+  $('html, body').stop().animate({
+    scrollTop: $(window).scrollTop() - distance * delta
+  }, time);
+}
+
 var createObs = function createObs(containers) {
   var options = {
     root: null,
